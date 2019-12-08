fee5a182b17efbc7971801958fd9e391735056d5
Bartłomiej Sienko <bartek_sk@interia.pl>
2019-12-08 22:23:03 +0100

    Updates

---
+++ precache-manifest.ccd25c9bb937847f0cb1bbe30e4edb0a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99c160add0a3ce525690eb9914e1e9ee",
+    "url": "/Air-Quality-Poland/index.html"
+  },
+  {
+    "revision": "16f9627591c65749438b",
+    "url": "/Air-Quality-Poland/static/js/2.849cf2ef.chunk.js"
+  },
+  {
+    "revision": "8a259117fe496d56e08d",
+    "url": "/Air-Quality-Poland/static/js/main.b15eb5ac.chunk.js"
+  },
+  {
+    "revision": "9a3820d290945451dc2d",
+    "url": "/Air-Quality-Poland/static/js/runtime-main.7bfbd0ef.js"
+  },
+  {
+    "revision": "fe68b692a67bf8ac6e232392cd04afe6",
+    "url": "/Air-Quality-Poland/static/media/pollution.fe68b692.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Air-Quality-Poland/precache-manifest.ccd25c9bb937847f0cb1bbe30e4edb0a.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Air-Quality-Poland/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
